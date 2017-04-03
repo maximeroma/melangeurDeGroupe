2650bf50e8fd1c044bc02b789f4e115807bba96a
romamaxime <roma.maxime1994@gmail.com>
2017-04-03 16:50:33 +0200

    add localStorage (bug!)

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
-$(document).ready(function(){
 
-
-var personne = ["Jordy", "Florian", "Mouhad", "Raphael", "Marco", "Dimitri", "Emilie", "Julien", "Franck", "Romain", "Aurelie", 
-					"Oceane", "Morel", "Gregory", "Maxime"];
 
 var groupe = [];
 var random;
+var numberOfTeam = 0;
 
-var j = 1;
 
-// création du nombre de groupe et injection des titres de colonnes 
+function save() {
+	localStorage.setItem('groupe', JSON.stringify(groupe));
+	localStorage.setItem('numberOfTeam', JSON.stringify(numberOfTeam));
+};
 
-var numberOfTeam = parseInt(prompt("Combien d'équipe?"));
 
-for(var n = 0; n < numberOfTeam; n++){
-	$('#numberTeam').append("<th>Groupe</th>")
-}
+function load() {
+	var groupe = localStorage.getItem('groupe');	
+	var numberOfTeam = localStorage.getItem('numberOfTeam');
+};
+
+
+
+var personne = ["Jordy", "Florian", "Mouhad", 
+				"Raphael", "Marco", "Dimitri", 
+				"Emilie", "Julien", "Franck", 
+				"Romain", "Aurelie", "Oceane", 
+				"Morel", "Gregory", "Maxime"];
 
 
 // creation du nouveau tableau aléatoire à partir du tableau personne
 
+var createNewArray = function(){
 	for(var i = personne.length; i > 0; i--){
 		random = Math.floor(Math.random() * i);
 		//console.log(personne);
@@ -27,20 +35,57 @@ for(var n = 0; n < numberOfTeam; n++){
 		groupe.push(personne[random]);
 		personne.splice(random, 1);
 		console.log(groupe);
-	
-}
+	}
+};
+
+// création du nombre de groupe et injection des titres de colonnes 
+
+var addHeadTable = function(){
+	numberOfTeam = parseInt(prompt("Combien d'équipe?"));
+	for(var n = 0; n < parseInt(numberOfTeam); n++){
+		$('#headTable').append("<th>Groupe</th>");
+	}
+};
+
 
 // injection des personnes dans le tableau dynamique (cf annuaire) 
+	
+var createDynamicTable = function(){
 
-	var addGroupe = function(){
-		$('tbody').append("<tr></tr>");
-		for( j ; j <= groupe.length; j++ ){
-			$("tr").last().append("<td>" + groupe[j - 1] + "</td>");
+	$('tbody').append("<tr></tr>");
+	for( var j = 1 ; j <= groupe.length; j++ ){
+		$("tr").last().append("<td>" + groupe[j - 1] + "</td>");
 			if(j % numberOfTeam === 0) {
 				console.log("OK");
 				$('tbody').append("<tr></tr>");
-			}
-		}
-	}
-addGroupe();
+			}	
+	};
+};
+
+
+$(document).ready(function(){
+
+	
+
+
+
+	// Quand je clique sur le bouton 'Shake' le tableau se mélange
+
+	
+	$('#button').click(function(){
+		load();
+		createNewArray();
+		addHeadTable();
+		createDynamicTable();
+		save();
+	});  
+
+	
+
+	/*	
+	createNewArray();
+	addHeadTable();
+	createDynamicTable();
+	*/
+
 }); 
